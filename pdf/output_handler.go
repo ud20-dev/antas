5a284d0dfe02a44bfcd3393bc436868282f243dc
gitmobkab <richmondaurel77@gmail.com>
2026-07-08 14:29:34 +0000

    Refactor GetPDFOutputPath to use timestamp and hash for output directory structure

--- pdf/output_handler.go
+++ pdf/output_handler.go
@@ -3,30 +3,31 @@ package pdf
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"os"
 	"path/filepath"
-
-	"github.com/google/uuid"
+	"time"
 )
 
-
-/* 
+/*
 this function takes a path to a pdf file create the folder structure and returns where the output image should be saved.
 since we render one image by page.
 the output path will follow this convention:
 
-{TEMP_DIR}/{UUID}/{hash_of_pdf_file}/
+	{TEMP_DIR}/{hash_of_pdf_file}/{when}. 
+	${when} = {now.UnixTime}-{CurrentPID}
+
 
 the program calling is expected to extend the path with the page number like so:
 
-{TEMP_DIR}/{UUID}/{hash_of_pdf_file}/page_{page_number}.png (,jpeg or any other format supported by the image package)
-	
+	{TEMP_DIR}/{hash_of_pdf_file}/{when}/page_{page_number}.png (,jpeg or any other format supported by the image package)
+
 
 For example, assuming we're in a linux environment
 
 #IN : "./test_samples/idiot.pdf"
 
-#OUT: "/tmp/3c82a360-6678-48d7-923a-8cb25a895742/hash_of_pdf_file/"
+#OUT: "/tmp/096c8484d1d42f3ca41c4952b5eabd69bda0fd04508c03b87d58608d84089911/1783520643-24423"
 */ 
 func GetPDFOutputPath(pdfPath string) (string, error) {
 	data, err := os.ReadFile(pdfPath)
@@ -34,8 +35,14 @@ func GetPDFOutputPath(pdfPath string) (string, error) {
 		return "", err
 	}
 
+	when := fmt.Sprintf("%d-%d", time.Now().Unix(), os.Getpid())
+
 	hash := sha256.Sum256(data)
-	outputPath := filepath.Join(os.TempDir(), uuid.NewString(), hex.EncodeToString(hash[:]))
+	outputPath := filepath.Join(
+		os.TempDir(), 
+		hex.EncodeToString(hash[:]),
+		when,
+	)
 	os.MkdirAll(outputPath, os.ModePerm)
 	return outputPath, nil
 }
